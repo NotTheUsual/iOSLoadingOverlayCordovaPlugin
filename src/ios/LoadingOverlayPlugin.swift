66fec90b1b719011000824e3e3c4a95a88348159
James Hunter <james.hunter@me.com>
2016-07-13 16:31:42 +0100

    Refactor

--- src/ios/LoadingOverlayPlugin.swift
+++ src/ios/LoadingOverlayPlugin.swift
@@ -62,34 +62,52 @@ class RoundUpLoadingOverlay: UIVisualEffectView {
     }
     
     convenience init(message: String, frame: CGRect) {
-        let blurEffect = UIBlurEffect(style: .Dark)
         self.init(frame: frame)
+        let blurEffect = UIBlurEffect(style: .Dark)
         self.effect = blurEffect
 
+        let label = RoundUpLoadingOverlay.makelabel(message: message)
+        self.addSubview(label)
+        let labelConstraints = RoundUpLoadingOverlay.constraints(for: label, in: self)
+
+        let spinner = RoundUpLoadingOverlay.makeSpinner()
+        self.addSubview(spinner)
+        let spinnerConstraints = RoundUpLoadingOverlay.constraints(for: spinner, in: self)
+
+        NSLayoutConstraint.activateConstraints(labelConstraints + spinnerConstraints)
+
+        spinner.startAnimating()
+    }
+    
+    private static func makelabel(message message: String) -> UILabel {
         let label = UILabel()
         label.text = message
         label.textColor = UIColor.whiteColor()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.lineBreakMode = .ByWordWrapping
         label.numberOfLines = 0
         label.font = UIFont.boldSystemFontOfSize(20.0)
         label.textAlignment = .Center
-        self.addSubview(label)
-
-        let labelCentreConstraint = NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0)
-        let labelTopConstraint = NSLayoutConstraint(item: label, attribute: .TopMargin, relatedBy: .Equal, toItem: self, attribute: .TopMargin, multiplier: 1.0, constant: 100)
-        let labelLeftConstraint = NSLayoutConstraint(item: label, attribute: .LeftMargin, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1.0, constant: 20.0)
-        let labelRightConstraint = NSLayoutConstraint(item: label, attribute: .RightMargin, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1.0, constant: 20.0)
-
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    private static func makeSpinner() -> UIActivityIndicatorView {
         let spinner = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
         spinner.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(spinner)
-
-        let spinnerCentreXConstraint = NSLayoutConstraint(item: spinner, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0)
-        let spinnerCentreYConstraint = NSLayoutConstraint(item: spinner, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: 0)
-
-        NSLayoutConstraint.activateConstraints([labelCentreConstraint, labelTopConstraint, labelLeftConstraint, labelRightConstraint, spinnerCentreXConstraint, spinnerCentreYConstraint])
-
-        spinner.startAnimating()
+        return spinner
+    }
+    
+    private static func constraints(for label: UILabel, in view: UIView) -> [NSLayoutConstraint] {
+        let labelCentreConstraint = NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0)
+        let labelTopConstraint = NSLayoutConstraint(item: label, attribute: .TopMargin, relatedBy: .Equal, toItem: view, attribute: .TopMargin, multiplier: 1.0, constant: 100)
+        let labelLeftConstraint = NSLayoutConstraint(item: label, attribute: .LeftMargin, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 20.0)
+        let labelRightConstraint = NSLayoutConstraint(item: label, attribute: .RightMargin, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: 20.0)
+        return [labelCentreConstraint, labelTopConstraint, labelLeftConstraint, labelRightConstraint]
+    }
+    
+    private static func constraints(for spinner: UIActivityIndicatorView, in view: UIView) -> [NSLayoutConstraint] {
+        let spinnerCentreXConstraint = NSLayoutConstraint(item: spinner, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0)
+        let spinnerCentreYConstraint = NSLayoutConstraint(item: spinner, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0)
+        return [spinnerCentreXConstraint, spinnerCentreYConstraint]
     }
 }
